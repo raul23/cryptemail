91393a40d596db6c2d1be754c059e76b668c463d
Raul <rchfe23@gmail.com>
2021-07-13 01:23:00 -0400

    Add configs

---
+++ monitoring/configs/default_config.py
@@ -0,0 +1,26 @@
+# ==================
+# 1. General options
+# ==================
+
+# 1.1 General control flags
+# =========================
+quiet = False
+verbose = False
+
+# 1.2 Miscellaneous options
+# =========================
+logging_level = 'info'
+logging_formatter = 'only_msg'
+
+# ===============
+# 2. Edit options
+# ===============
+edit = False
+app = None
+reset = False
+cfg = 'main'
+
+# ==================
+# 3. Monitor options
+# ==================
+start_monitoring = False
