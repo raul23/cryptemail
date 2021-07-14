395736afbd1e6da70ba1dad834598b4e7d5889df
Raul <rchfe23@gmail.com>
2021-07-14 15:51:24 -0400

    monitor: add Service

--- monitoring/scripts/monitor.py
+++ monitoring/scripts/monitor.py
@@ -15,26 +15,66 @@ AGENT_SCRIPT = 'agent.py'
 # =====================
 # Default config values
 # =====================
+APP = default_cfg.app
 EDIT = default_cfg.edit
 LOGGING_FORMATTER = default_cfg.logging_formatter
 LOGGING_LEVEL = default_cfg.logging_level
 RESET = default_cfg.reset
 
 
-class Agent:
-    def __init__(self, stealth=False):
-        self.stealth = stealth
-        if stealth:
-            logger.info('Stealth mode enabled')
-            self.agent_name = 'com.mac.load.agent'
-        else:
+class Service:
+    def __init__(self, service_type='agent'):
+        self.service_type = service_type
+        if service_type == 'agent':
             self.agent_name = 'com.mac-monitoring.agent'
+        else:
+            raise NotImplementedError(
+                f"Service type '{service_type}' not supported. Only 'agent' "
+                "is supported as a service")
+        self.plist_path = os.path.expanduser(
+            f'~/Library/LaunchAgents/{self.agent_name}.plist')
 
-    def load(self):
-        pass
+    def abort(self):
+        raise NotImplementedError(red('abort() is not implemented!'))
 
-    def unload(self):
-        pass
+    def pause(self):
+        logger.debug(f"Pausing {self.service_type} '{self.agent_name}'...")
+        cmd = f'launchctl unload {self.plist_path}'
+        result = subprocess.run(shlex.split(cmd), capture_output=True)
+        return check_result(
+            result,
+            error_msg=f"The {self.service_type} couldn't be disabled",
+            valid_msg=f'{self.service_type} disabled',
+            err_key='not find')
+
+    def start(self):
+        logger.debug(f"Starting {self.service_type} '{self.agent_name}'...")
+        script_path = os.path.join(os.path.dirname(scripts.__file__), AGENT_SCRIPT)
+        plist_content = f'''<?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC -//Apple Computer//DTD PLIST 1.0//EN http://www.apple.com/DTDs/PropertyList-1.0.dtd >
+        <plist version="1.0">
+          <dict>
+            <key>Label</key>
+            <string>{self.agent_name}</string>
+            <key>Program</key>
+            <string>{script_path}</string>
+            <key>KeepAlive</key>
+            <true/>
+          </dict>
+        </plist>
+            '''
+        tmp_file_plist = tempfile.mkstemp(suffix='.plist')[1]
+        with open(tmp_file_plist, 'w') as f:
+            f.write(plist_content)
+        copy(tmp_file_plist, self.plist_path)
+        remove_file(tmp_file_plist)
+        cmd = f'launchctl load {self.plist_path}'
+        result = subprocess.run(shlex.split(cmd), capture_output=True)
+        return check_result(
+            result,
+            error_msg=f"The {self.service_type} couldn't be started",
+            valid_msg=f'{self.service_type} started',
+            skip_key='already loaded')
 
 
 def check_result(result, error_msg, valid_msg, err_key=None, skip_key=None):
@@ -45,40 +85,8 @@ def check_result(result, error_msg, valid_msg, err_key=None, skip_key=None):
         logger.error(f"{error()} {error_msg}: {stderr}")
         return 1
     else:
-        logger.info(valid_msg)
+        logger.info(green(valid_msg))
         return 0
-
-
-def get_plist_path(agent_name):
-    return os.path.expanduser(f'~/Library/LaunchAgents/{agent_name}.plist')
-
-
-def load_agent(main_cfg):
-    agent_name = main_cfg.agent_name
-    logger.debug(f"Loading agent '{agent_name}'...")
-    plist_path = get_plist_path(agent_name)
-    script_path = os.path.join(os.path.dirname(scripts.__file__), AGENT_SCRIPT)
-    plist_content = f'''<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC -//Apple Computer//DTD PLIST 1.0//EN http://www.apple.com/DTDs/PropertyList-1.0.dtd >
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>{agent_name}</string>
-    <key>Program</key>
-    <string>{script_path}</string>
-    <key>KeepAlive</key>
-    <true/>
-  </dict>
-</plist>
-    '''
-    tmp_file_plist = tempfile.mkstemp(suffix='.plist')[1]
-    with open(tmp_file_plist, 'w') as f:
-        f.write(plist_content)
-    copy(tmp_file_plist, plist_path)
-    remove_file(tmp_file_plist)
-    cmd = f'launchctl load {plist_path}'
-    result = subprocess.run(shlex.split(cmd), capture_output=True)
-    return check_result(result, "The agent couldn't be loaded", 'Agent loaded', skip_key='already loaded')
 
 
 def setup_argparser():
@@ -87,8 +95,8 @@ def setup_argparser():
         description=f'''
 Script for monitoring your Mac.
 
-{get_important_msg()}''',
-        usage=get_usage(__file__),
+{important()}''',
+        usage=usage(__file__),
         add_help=False,
         # ArgumentDefaultsHelpFormatter
         # HelpFormatter
@@ -98,8 +106,7 @@ Script for monitoring your Mac.
     # ===============
     # General options
     # ===============
-    general_group = parser.add_argument_group(
-        f"{COLORS['YELLOW']}General options{COLORS['NC']}")
+    general_group = parser.add_argument_group(f"{yellow('General options')}")
     general_group.add_argument('-h', '--help', action='help',
                                help='Show this help message and exit.')
     general_group.add_argument(
@@ -118,66 +125,49 @@ Script for monitoring your Mac.
         '--log-level', dest='logging_level',
         choices=['debug', 'info', 'warning', 'error'],
         help='Set logging level for all loggers.'
-             + get_default_message(LOGGING_LEVEL))
+             + default(LOGGING_LEVEL))
     # TODO: explain each format
     general_group.add_argument(
         '--log-format', dest='logging_formatter',
         choices=['console', 'simple', 'only_msg'],
         help='Set logging formatter for all loggers.'
-             + get_default_message(LOGGING_FORMATTER))
+             + default(LOGGING_FORMATTER))
     # ================
     # Edit/reset files
     # ================
     edit_group = parser.add_argument_group(
-        f"{COLORS['YELLOW']}Edit/reset a configuration file{COLORS['NC']}")
+        f"{yellow('Edit/reset a configuration file')}")
     parser_edit_mutual_group = edit_group.add_mutually_exclusive_group()
     parser_edit_mutual_group.add_argument(
         "-e", "--edit", choices=[LOG_CFG, MAIN_CFG],
         help='Edit a configuration file, either the main configuration file '
              f'(`{MAIN_CFG}`) or the logging configuration file (`{LOG_CFG}`).'
-             + get_default_message(EDIT))
+             + default(EDIT))
     edit_group.add_argument(
         "--app", dest="app", default=None,
         help='''Name of the application to use for editing the file. If no 
             name is given, then the default application for opening this type of
-            file will be used.''')
+            file will be used.''' + default(APP))
     parser_edit_mutual_group.add_argument(
         '-r', '--reset', choices=[LOG_CFG, MAIN_CFG],
         help='Reset a configuration file to factory values. It can either be '
              'the main configuration file (`{MAIN_CFG}`) or the logging '
-             'configuration file (`{LOG_CFG}`).' + get_default_message(RESET))
+             'configuration file (`{LOG_CFG}`).' + default(RESET))
     # ==================
     # Monitoring options
     # ==================
-    monitor_group = parser.add_argument_group(
-        f"{COLORS['YELLOW']}Monitoring options{COLORS['NC']}")
-    monitor_group.add_argument(
+    monitor_group = parser.add_argument_group(f"{yellow('Monitoring options')}")
+    parser_mutual_group = monitor_group.add_mutually_exclusive_group()
+    parser_mutual_group.add_argument(
         '-a', '--abort-monitoring', action="store_true",
         help='Abort system monitoring.')
-    monitor_group.add_argument(
+    parser_mutual_group.add_argument(
         '-p', '--pause-monitoring', action="store_true",
         help='Pause system monitoring.')
-    monitor_group.add_argument(
+    parser_mutual_group.add_argument(
         '-s', '--start-monitoring', action="store_true",
         help='Start system monitoring.')
-    monitor_group.add_argument(
-        '-f', '--force', action="store_true",
-        help='Forcibly execute the given operation (e.g. pause) on all '
-             'loaded monitoring agents no matter their type (stealth or not).')
-    monitor_group.add_argument(
-        '--stealth', action="store_true",
-        help='Enable stealth system monitoring, i.e. make the monitoring as '
-             'transparent as possible.')
     return parser
-
-
-def unload_agent(main_cfg):
-    logger.debug(f"Unloading agent '{main_cfg.agent_name}'...")
-    plist_path = get_plist_path(main_cfg.agent_name)
-    cmd = f'launchctl unload {plist_path}'
-    result = subprocess.run(shlex.split(cmd), capture_output=True)
-    return check_result(result, "The agent couldn't be unloaded",
-                        'Agent unloaded', 'not find')
 
 
 def main():
@@ -187,8 +177,8 @@ def main():
         args = parser.parse_args()
         # TODO: find if you can do it in setup_argparser()
         if args.app and not args.edit:
-            print(f"\n{COLORS['RED']}error: argument -a/--app: required with "
-                  f"argument -e/--edit{COLORS['NC']}")
+            msg = red('error: argument -a/--app: required with argument -e/--edit')
+            print(f"\n{msg}")
             return 1
         # Get main cfg dict
         # TODO: important, check if an option is defined more than once
@@ -203,19 +193,17 @@ def main():
                   logging_level=main_cfg.logging_level,
                   logging_formatter=main_cfg.logging_formatter)
         process_returned_values(returned_values)
-        if main_cfg.stealth:
-            logger.info('Stealth mode enabled')
-            main_cfg.agent_name = 'com.mac.load.agent'
-        else:
-            main_cfg.agent_name = 'com.mac-monitoring.agent'
+        service = Service()
         if main_cfg.edit:
             exit_code = edit_file(main_cfg.edit, main_cfg.app, get_configs_dirpath())
         elif main_cfg.reset:
             exit_code = reset_file(main_cfg.reset, get_configs_dirpath())
         elif main_cfg.start_monitoring:
-            exit_code = load_agent(main_cfg)
+            exit_code = service.start()
         elif main_cfg.pause_monitoring:
-            exit_code = unload_agent(main_cfg)
+            exit_code = service.pause()
+        elif main_cfg.abort_monitoring:
+            exit_code = service.abort()
     except AssertionError as e:
         # TODO (IMPORTANT): use same logic as in other project
         # TODO: add KeyboardInterruptError
