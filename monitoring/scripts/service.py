88aef7d835fc43750706eb91113fce98b742284b
Raul <rchfe23@gmail.com>
2021-07-14 20:33:44 -0400

    service: add test command

--- monitoring/scripts/service.py
+++ monitoring/scripts/service.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+import os
+import time
+import sys
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        last = sys.argv[1]
+        event_msg_contains = sys.argv[2]
+        os.system('echo "{} {}" >> '.format(last, event_msg_contains) +
+                  os.path.expanduser('~/foostore.txt'))
+        time.sleep(1)
+        """
+        os.system(f"log show --last {last} --style syslog --predicate "
+                  f"'eventMessage contains \"{event_msg_contains}\"'")
+        """
