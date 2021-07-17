5031052477e5591ac82adc3500227405c05eb10c
Raul <rchfe23@gmail.com>
2021-07-16 21:25:27 -0400

    service: write more info in file

--- monitoring/scripts/service.py
+++ monitoring/scripts/service.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import os
 import shlex
 import subprocess
 import sys
+import time
 
 
 def send_alert():
@@ -9,19 +11,33 @@ def send_alert():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        last = sys.argv[1]
-        predicate = sys.argv[2]
-        cmd = "log show --last {} --style syslog --predicate \'{}\'".format(
-                last, predicate)
-        try:
-            result = subprocess.check_output(shlex.split(cmd),
-                                             stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            sys.exit(1)
-        import ipdb
-        ipdb.set_trace()
-        result = result.decode()
-        if result.count('\n') > 1:
-            pass
+    os.system('echo "Starting!" >> ' + os.path.expanduser('~/foostore.txt'))
+    if len(sys.argv) == 2 or True:
+        # configs_path = sys.argv[1]
+        configs_path = os.path.expanduser('~/mac-monitoring')
+        sys.path.append(configs_path)
+        import config
+        sleeping = 5
+        while True:
+            os.system('echo "Executing log command" >> ' + os.path.expanduser('~/foostore.txt'))
+            last = '220m'
+            cmd = "log show --last {} --style syslog --predicate \'{}\'".format(
+                    last, config.predicate)
+            try:
+                result = subprocess.check_output(shlex.split(cmd),
+                                                 stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as e:
+                os.system('echo "Error with log command!" >> ' + os.path.expanduser('~/foostore.txt'))
+                sys.exit(1)
+            result = result.decode()
+            # Don't include first '\n' which is for the row "Timestamp (process)[PID]"
+            failed_login_counts = result.count('\n') - 1
+            if failed_login_counts:
+                os.system('echo "ALERT!" >> ' + os.path.expanduser('~/foostore.txt'))
+                os.system('echo "{}" failed login >> '.format(failed_login_counts) + os.path.expanduser('~/foostore.txt'))
+            else:
+                os.system('echo "Nothing to report!" >> ' + os.path.expanduser('~/foostore.txt'))
+            os.system('echo "Sleeping for {}" >> '.format(sleeping) + os.path.expanduser('~/foostore.txt'))
+            time.sleep(sleeping)
+    os.system('echo "Ending! Very odd!" >> ' + os.path.expanduser('~/foostore.txt'))
     sys.exit(0)
