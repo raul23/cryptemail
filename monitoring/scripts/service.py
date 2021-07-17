1cfa3bfc67dcc450c5ead4bdfd631ad9e775e1c3
Raul <rchfe23@gmail.com>
2021-07-16 22:50:50 -0400

    service: add db functions and (kind of) logs

--- monitoring/scripts/service.py
+++ monitoring/scripts/service.py
@@ -1,43 +1,108 @@
 #!/usr/bin/env python
 import os
 import shlex
+import sqlite3
 import subprocess
 import sys
 import time
 
+# TODO: important, add logger
+ALERTS_PATH = os.path.expanduser('~/alerts.txt')
+LOGS_PATH = os.path.expanduser('~/logs.txt')
 
-def send_alert():
-    pass
+
+def alert(msg):
+    os.system('echo {} >> {}'.format(msg, ALERTS_PATH))
 
 
-if __name__ == '__main__':
-    os.system('echo "Starting!" >> ' + os.path.expanduser('~/foostore.txt'))
-    if len(sys.argv) == 2 or True:
+def connect_db(db_path, autocommit=False):
+    try:
+        if autocommit:
+            # If isolation_level is None, it will leave the underlying sqlite3
+            # library operating in autocommit mode
+            # Ref.: https://bit.ly/2mg5Hie
+            conn = sqlite3.connect(db_path, isolation_level=None)
+        else:
+            conn = sqlite3.connect(db_path)
+    except sqlite3.Error:
+        raise
+    else:
+        return conn
+
+
+def create_db(db_filepath, schema_filepath, overwrite_db=False):
+    log('Creating database...')
+    db_filepath = os.path.expanduser(db_filepath)
+    schema_filepath = os.path.expanduser(schema_filepath)
+    db_exists = os.path.exists(db_filepath)
+
+    if overwrite_db and db_exists:
+        os.remove(db_filepath)
+
+    retcode = 0
+    if not db_exists or overwrite_db:
+        try:
+            with sqlite3.connect(db_filepath) as conn:
+                f = open(schema_filepath, 'rt')
+                schema = f.read()
+                conn.executescript(schema)
+                f.close()
+        except (IOError, sqlite3.OperationalError) as e:
+            # logger.error("<color>{}</color>".format(get_error_msg(e)))
+            log(e)
+            raise
+        else:
+            # logger.info('Database created!')
+            log('Database created!')
+    else:
+        # logger.warning("Database '{}' already exists!".format(db_filepath))
+        log("Database '{}' already exists!".format(db_filepath))
+        retcode = 1
+    return retcode
+
+
+def log(msg):
+    os.system('echo {} >> {}'.format(msg, LOGS_PATH))
+
+
+def main():
+    log('Starting!')
+    number_of_args = 2
+    if len(sys.argv) == number_of_args or True:
         # configs_path = sys.argv[1]
         configs_path = os.path.expanduser('~/mac-monitoring')
         sys.path.append(configs_path)
         import config
-        sleeping = 5
+        sleeping = 2
+        last = '520m'
         while True:
-            os.system('echo "Executing log command" >> ' + os.path.expanduser('~/foostore.txt'))
-            last = '220m'
+            log('Executing log command')
             cmd = "log show --last {} --style syslog --predicate \'{}\'".format(
                     last, config.predicate)
             try:
                 result = subprocess.check_output(shlex.split(cmd),
                                                  stderr=subprocess.STDOUT)
             except subprocess.CalledProcessError as e:
-                os.system('echo "Error with log command!" >> ' + os.path.expanduser('~/foostore.txt'))
+                log('Error with log command!')
                 sys.exit(1)
             result = result.decode()
             # Don't include first '\n' which is for the row "Timestamp (process)[PID]"
             failed_login_counts = result.count('\n') - 1
             if failed_login_counts:
-                os.system('echo "ALERT!" >> ' + os.path.expanduser('~/foostore.txt'))
-                os.system('echo "{}" failed login >> '.format(failed_login_counts) + os.path.expanduser('~/foostore.txt'))
+                alert('ALERT: {} failed login detected!'.format(failed_login_counts))
             else:
-                os.system('echo "Nothing to report!" >> ' + os.path.expanduser('~/foostore.txt'))
-            os.system('echo "Sleeping for {}" >> '.format(sleeping) + os.path.expanduser('~/foostore.txt'))
+                log('Nothing to report!')
+            log('Sleeping for {} seconds'.format(sleeping))
             time.sleep(sleeping)
-    os.system('echo "Ending! Very odd!" >> ' + os.path.expanduser('~/foostore.txt'))
+    else:
+        log('Wrong number ({}) of arguments. There should be {} arguments'.format(
+            len(sys.argv), number_of_args))
+    log('Ending! Very odd!')
     sys.exit(0)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        log('Ctrl+c detected!')
