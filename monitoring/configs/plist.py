5c26962b0d30805b2e2ab1c7a52a2ff5b909bb2a
Raul <rchfe23@gmail.com>
2021-07-16 20:12:31 -0400

    plist: use configs path

--- monitoring/configs/plist.py
+++ monitoring/configs/plist.py
@@ -1,4 +1,4 @@
-test_plist_content = '''<?xml version="1.0" encoding="UTF-8"?>
+test1_plist_content = '''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC -//Apple Computer//DTD PLIST 1.0//EN http://www.apple.com/DTDs/PropertyList-1.0.dtd >
 <plist version="1.0">
   <dict>
@@ -6,6 +6,24 @@ test_plist_content = '''<?xml version="1.0" encoding="UTF-8"?>
     <string>{service_name}</string>
     <key>Program</key>
     <string>{script_path}</string>
+    <key>KeepAlive</key>
+    <true/>
+  </dict>
+</plist>
+'''
+
+test2_plist_content = '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC -//Apple Computer//DTD PLIST 1.0//EN http://www.apple.com/DTDs/PropertyList-1.0.dtd >
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>{service_name}</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>{script_path}</string>
+      <string>20m</string>
+      <string>"Failed to authenticate"</string>
+    </array>
     <key>KeepAlive</key>
     <true/>
   </dict>
@@ -21,8 +39,7 @@ plist_content = '''<?xml version="1.0" encoding="UTF-8"?>
     <key>ProgramArguments</key>
     <array>
       <string>{script_path}</string>
-      <string>20m</string>
-      <string>"Failed to authenticate"</string>
+      <string>{configs_path}</string>
     </array>
     <key>KeepAlive</key>
     <true/>
