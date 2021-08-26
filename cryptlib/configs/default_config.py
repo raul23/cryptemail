8c78678c8959327f87e2f10abba9e454c69734fa
Raul <rchfe23@gmail.com>
2021-08-26 01:32:12 -0400

    default_config: remove SYMMETRIC

--- cryptlib/configs/default_config.py
+++ cryptlib/configs/default_config.py
@@ -5,26 +5,17 @@
 # ==============
 # Common options
 # ==============
-# Home directory where the keys will be saved
+# Home directory where the keys (e.g. GnuPG) will be saved
+# TODO: important, add support to read HOMEDIR from environment variable GNUPGHOME
+# if HOMEDIR is None or ''
 HOMEDIR = '/path/to/homedir'
 ASYMMETRIC = {
     'name': 'asymmetric',
-    # Fingerprint associated with encryption and decryption
-    'encryption_fingerprint': 'YOUR_ENCRYPTION_FINGERPRINT',
-    # Fingerprint associated with signature
+    # Recipient's fingerprint (for encryption when sending an email)
+    'recipient_fingerprint': 'RECIPIENT_FINGERPRINT',
+    # Your signature fingerprint
     'signature_fingerprint': 'YOUR_SIGNATURE_FINGERPRINT'
 }
-SYMMETRIC = {
-    'name': 'symmetric',
-    'cipher_algo': 'AES256',
-    'digest_algo': 'SHA512',
-    'compress_algo': 'default',
-    # If no symmetric encryption password found saved locally, prompt password
-    'prompt_symmetric_password': True
-}
-# Use Pinentry Mac for entering your passphrase when generating keys, signing
-# or decrypting emails
-USE_PINENTRY_MAC = True
 # If the passphrase can't be found saved locally, prompt for it
 # Passphrase will be used for decryption
 PROMPT_PASSPHRASE = True
@@ -33,8 +24,8 @@ PROMPT_PASSPHRASE = True
 # Connection options
 # ==================
 # NOTE: both addresses can be the same
-SENDER_EMAIL_ADDRESS = 'YOUR_SENDER_EMAIL_ADDRESS@mail.com'
-READER_EMAIL_ADDRESS = 'YOUR_READER_EMAIL_ADDRESS@mail.com'
+SENDER_EMAIL_ADDRESS = 'YOUR_SENDER_EMAIL_ADDRESS@email.com'
+READER_EMAIL_ADDRESS = 'YOUR_READER_EMAIL_ADDRESS@email.com'
 
 # googleapi can be used both for sending and reading emails
 # IMPORTANT: token-based authentication is only supported for gmail addresses
@@ -62,7 +53,7 @@ smtp = {
     'smtp_server': 'smtp.gmail.com',
     # If no email password (e.g. from your gmail account) found saved locally,
     # prompt email password
-    'prompt_email_password': True,
+    'prompt_email_password': True
 }
 
 # ==================================
@@ -75,24 +66,15 @@ send_emails = {
     # FROM and TO information
     'sender_email_address': SENDER_EMAIL_ADDRESS,  # FROM
     'receiver_email_address': READER_EMAIL_ADDRESS,  # TO
-    # If keys not found saved locally, prompt the user to enter data useful
-    # for generating the keys
-    'prompt_generate_keys': True,
-    # Use the same encryption keys for signing also.
-    # IMPORTANT: it is highly recommended to use separate keys for encrypting
-    # and signing
-    'reuse_keys': True,
     # Sign and encrypt in a single pass. Otherwise, sign first and then encrypt
     # as separate processes
     'use_single_pass': False,
-    # Sign emails
+    # Signature options
     'signature': {
         'program': 'PGP',
         'enable_signature': True,
     },
     # Encryption options
-    # NOTE: the emails in the inbox will remain encrypted and will only be
-    # decrypted locally
     'encryption': {
         'program': 'PGP',
         'encryption_type': ASYMMETRIC
@@ -101,18 +83,13 @@ send_emails = {
 
 # Config options for reading emails
 read_emails = {
-    # How to connect to the email server: googleapi or smtp
+    # How to connect to the email server for reading emails: googleapi or smtp
     'connection_method': googleapi,
     'reader_email_address': READER_EMAIL_ADDRESS,
-    'show_first_unread_emails': 10,
-    # Delete emails from the inbox after reading them
-    'delete_emails_read': False,
-    # If True, every new emails will be saved locally
-    'save_emails': False,
     'add_decryption_results': False,
     # Folders for saving emails
     'valid_emails_dirpath': '/path/to/valid/emails/',
-    'invaid_emails_dirpath': '/path/to/invalid/emails/',
+    'invalid_emails_dirpath': '/path/to/invalid/emails/',
     'unknown_emails_dirpath': '/path/to/unknown/emails/'
 }
 
@@ -125,5 +102,6 @@ test_encryption = True
 # Test message signature
 test_signature = True
 
-# Test connection to an email server either through googleapi or smtp
+# Test connection to an email server either through googleapi, smtp or None
+# If None, then no connection testing will done
 test_connection = googleapi
