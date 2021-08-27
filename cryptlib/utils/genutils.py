dae496c2a676a57100e877c512755398bd7685f9
Raul <rchfe23@gmail.com>
2021-08-27 17:15:15 -0400

    genutils: setup_log()

--- cryptlib/utils/genutils.py
+++ cryptlib/utils/genutils.py
@@ -1,6 +1,26 @@
 """General utilities
 """
 import argparse
+import codecs
+import importlib
+import json
+import logging.config
+import os
+import shutil
+import sys
+from collections import namedtuple, OrderedDict
+from logging import NullHandler
+from pathlib import Path
+from runpy import run_path
+
+from cryptlib.utils.logutils import (init_log, set_logging_field_width,
+                                     set_logging_formatter, set_logging_level)
+
+logger = init_log(__name__, __file__)
+# TODO: is next necessary? already done in init_log
+logger.addHandler(NullHandler())
+
+CFG_TYPES = ['main', 'log']
 
 COLORS = {
     'GREEN': '\033[0;36m',  # 32
@@ -76,8 +96,219 @@ class MyFormatter(argparse.RawDescriptionHelpFormatter):
             return ', '.join(parts)
 
 
+def get_config_dict(cfg_type='main', configs_dirpath=None):
+    return load_cfg_dict(get_config_filepath(cfg_type, configs_dirpath), cfg_type)
+
+
+def get_config_filepath(cfg_type='main', configs_dirpath=None):
+    if cfg_type == 'main':
+        cfg_filepath = get_main_config_filepath(configs_dirpath)
+    elif cfg_type == 'log':
+        cfg_filepath = get_logging_filepath(configs_dirpath)
+    else:
+        raise ValueError(f"Invalid cfg_type: {cfg_type}")
+    return cfg_filepath
+
+
+def get_settings(conf, cfg_type):
+    if cfg_type == 'log':
+        # set_logging_field_width(conf['logging'])
+        return conf['logging']
+    elif cfg_type == 'main':
+        _settings = {}
+        for opt_name, opt_value in conf.items():
+            if opt_name.startswith('__') and opt_name.endswith('__'):
+                continue
+            elif isinstance(opt_value, type(os)):
+                # e.g. import config
+                continue
+            else:
+                _settings.setdefault(opt_name, opt_value)
+        return _settings
+    else:
+        raise ValueError(f"Invalid cfg_type: {cfg_type}")
+
+
+def load_cfg_dict(cfg_filepath, cfg_type):
+
+    def _load_cfg_dict(cfg_filepath, cfg_type):
+        if file_ext == '.py':
+            cfg_dict = run_path(cfg_filepath)
+            cfg_dict = get_settings(cfg_dict, cfg_type)
+        elif file_ext == '.json':
+            cfg_dict = load_json(cfg_filepath)
+        else:
+            raise TypeError("Config file extension not supported: "
+                            f"{cfg_filepath}")
+        return cfg_dict
+
+    configs_dirpath = Path(cfg_filepath).parent
+    assert cfg_type in CFG_TYPES, f"Invalid cfg_type: {cfg_type}"
+    _, file_ext = os.path.splitext(cfg_filepath)
+    try:
+        cfg_dict = _load_cfg_dict(cfg_filepath, cfg_type)
+    except FileNotFoundError:
+        # Copy it from the default one
+        if cfg_type == 'main':
+            src = get_main_config_filepath(configs_dirpath, default_config=True)
+        else:
+            src = get_logging_filepath(configs_dirpath, default_config=True)
+        shutil.copy(src, cfg_filepath)
+        print(f"Config file created: {cfg_filepath}")
+        cfg_dict = _load_cfg_dict(cfg_filepath, cfg_type)
+    return cfg_dict
+
+
+def load_json(filepath, encoding='utf8'):
+    """Load JSON data from a file on disk.
+
+    If using Python version betwee 3.0 and 3.6 (inclusive), the data is
+    returned as :obj:`collections.OrderedDict`. Otherwise, the data is
+    returned as :obj:`dict`.
+
+    Parameters
+    ----------
+    filepath : str
+        Path to the JSON file which will be read.
+    encoding : str, optional
+        Encoding to be used for opening the JSON file in read mode (the default
+        value is '*utf8*').
+
+    Returns
+    -------
+    data : dict or collections.OrderedDict
+        Data loaded from the JSON file.
+
+    Raises
+    ------
+    OSError
+        Raised if any I/O related error occurs while reading the file, e.g. the
+        file doesn't exist.
+
+    References
+    ----------
+    `Are dictionaries ordered in Python 3.6+? (stackoverflow)`_
+
+    """
+    try:
+        with codecs.open(filepath, 'r', encoding) as f:
+            if sys.version_info.major == 3 and sys.version_info.minor <= 6:
+                data = json.load(f, object_pairs_hook=OrderedDict)
+            else:
+                data = json.load(f)
+    except OSError:
+        raise
+    else:
+        return data
+
+
+def override_config_with_args(main_config, args):
+
+    def process_user_args():
+
+        def get_opt_val(opt_name, cfg_dict, use_config=False):
+            if opt_name.find('.') != -1:
+                if use_config:
+                    keys = opt_name.split('.')
+                    opt_val = cfg_dict
+                    for k in keys:
+                        opt_val = opt_val[k]
+                else:
+                    opt_val = cfg_dict[opt_name]
+            else:
+                opt_val = cfg_dict.get(opt_name, 'not_found')
+            return opt_val
+
+        def set_opt_val(opt_name, opt_val, cfg_dict):
+            if opt_name.find('.') != -1:
+                keys = opt_name.split('.')
+                tmp_dict = cfg_dict
+                for i, k in enumerate(keys):
+                    if i == len(keys) - 1:
+                        break
+                    tmp_dict = tmp_dict[k]
+                tmp_dict[keys[-1]] = opt_val
+            else:
+                cfg_dict[opt_name] = opt_val
+            return opt_val
+
+        for arg_name, arg_val in list(user_args.items()):
+            arg_val = get_opt_val(arg_name, user_args)
+            config_val = get_opt_val(arg_name, main_config, use_config=True)
+            if arg_val is not None:
+                if arg_val != config_val:
+                    # User specified a value in the command-line/config file
+                    set_opt_val(arg_name, arg_val, main_config)
+                    if config_val == 'not_found':
+                        results.args_not_found_in_config.append((arg_name, config_val, arg_val))
+                    else:
+                        results.config_opts_overridden.append((arg_name, config_val, arg_val))
+                # else: user didn't change the config value (same as default one)
+            else:
+                if config_val == 'not_found':
+                    logger.debug("No value could be found for the "
+                                 f"argument '{arg_name}'")
+                    set_opt_val(arg_name, arg_val, main_config)
+                    results.args_not_found_in_config.append((arg_name, config_val, arg_val))
+
+    main_config = vars(main_config)
+    args = args.__dict__
+    results = namedtuple("results", "args_not_found_in_config config_opts_overridden msg")
+    results.args_not_found_in_config = []
+    results.config_opts_overridden = []
+    results.msg = 'Config options overridden by command-line arguments:\n'
+    user_args = args.copy()
+    process_user_args()
+    return results
+
+
 def prog_name(filename):
+    filename = Path(filename).name
     return filename.split('.py')[0]
+
+
+def setup_log(package=None, script_name=None, log_filepath=None,
+              configs_dirpath=None, quiet=False, verbose=False,
+              logging_level=None, logging_formatter=None, subcommand=None):
+    if type(package) == str:
+        package = importlib.import_module(package)
+    package_path = os.getcwd()
+    log_filepath = log_filepath if log_filepath else get_logging_filepath(configs_dirpath)
+    main_log_msg = f'Logging path: {log_filepath}'
+    # Get logging cfg dict
+    log_dict = load_cfg_dict(log_filepath, cfg_type='log')
+    # NOTE: if quiet and verbose are both activated, only quiet will have an effect
+    if not quiet:
+        if verbose:
+            # verbose supercedes logging_level
+            set_logging_level(log_dict, level='DEBUG')
+        else:
+            if logging_level:
+                logging_level = logging_level.upper()
+                set_logging_level(log_dict, level=logging_level)
+        if logging_formatter:
+            set_logging_formatter(log_dict, formatter=logging_formatter)
+        if subcommand:
+            size_longest_name = max([len(key) for key in log_dict['loggers'].keys()])
+            for log_name, _ in log_dict['loggers'].items():
+                if log_name.startswith(package.__name__) and subcommand in log_name:
+                    size_longest_name = max(size_longest_name, len(log_name))
+        else:
+            size_longest_name = None
+        set_logging_field_width(log_dict, size_longest_name)
+        # Load logging config dict
+        logging.config.dictConfig(log_dict)
+    # =============
+    # Start logging
+    # =============
+    if package:
+        script_name = script_name if script_name else package.__name__
+        logger.info("Running {} v{}".format(script_name,
+                                            package.__version__))
+    logger.info("Verbose option {}".format(
+        "enabled" if verbose else "disabled"))
+    logger.debug("Working directory: {}".format(package_path))
+    logger.debug(main_log_msg)
 
 
 # ------
@@ -109,3 +340,33 @@ def red(msg):
 
 def yellow(msg):
     return f"{color(msg)}"
+
+
+# -------------------------------
+# Configs: dirpaths and filepaths
+# -------------------------------
+def get_configs_dirpath():
+    from cryptlib.configs import __path__
+    return __path__._path[0]
+
+
+def get_logging_filepath(configs_dirpath=None, default_config=False):
+    configs_dirpath = get_configs_dirpath() if configs_dirpath is None else configs_dirpath
+    if default_config and not os.path.exists(os.path.join(configs_dirpath, 'default_logging.py')):
+        default_config = True
+        configs_dirpath = get_configs_dirpath()
+    if default_config:
+        return os.path.join(configs_dirpath, 'default_logging.py')
+    else:
+        return os.path.join(configs_dirpath, 'logging.py')
+
+
+def get_main_config_filepath(configs_dirpath=None, default_config=False):
+    configs_dirpath = get_configs_dirpath() if configs_dirpath is None else configs_dirpath
+    if default_config and not os.path.exists(os.path.join(configs_dirpath, 'default_config.py')):
+        default_config = True
+        configs_dirpath = get_configs_dirpath()
+    if default_config:
+        return os.path.join(configs_dirpath, 'default_config.py')
+    else:
+        return os.path.join(configs_dirpath, 'config.py')
