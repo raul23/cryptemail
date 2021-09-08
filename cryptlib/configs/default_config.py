69ad46c2228db0a479465fca6c53dd8e7a53da8d
Raul <rchfe23@gmail.com>
2021-09-07 23:48:30 -0400

    Update default_config.py

--- cryptlib/configs/default_config.py
+++ cryptlib/configs/default_config.py
@@ -7,17 +7,8 @@
 # ==============
 # Home directory where the keys (e.g. GnuPG) will be saved
 homedir = '/path/to/homedir'
-asymmetric = {
-    # Recipient's USER-ID, e.g. fingerprint
-    # For encryption when sending an email
-    'recipient_userid': 'RECIPIENT_USERID',
-    # Your signature (USER-ID)
-    'signature': 'YOUR_SIGNATURE'
-}
 interactive = False
-# If the passphrase can't be found saved locally, prompt for it
-# Passphrase will be used for decryption and signing
-prompt_passphrase = True
+prompt_passwords = True
 
 # ===============
 # General options
@@ -36,21 +27,17 @@ app = None
 # Connection options
 # ==================
 mailbox_address = 'your_mailbox@address.com'
+# How to connect to the email server: googleapi or smtp_imap
+connection_method = 'smtp_imap'
 
 # googleapi can be used both for sending and reading emails
 # IMPORTANT: token-based authentication is only supported for gmail addresses
 # The use of tokens is more secure than using an email password
 googleapi = {
-    'sender_auth': {
-        'credentials_path': '/path/to/credentials.json',
-        # Scopes for the gmail google API
-        'scopes': ['https://www.googleapis.com/auth/gmail.modify'],
-    },
-    'reader_auth': {
-        'credentials_path': '/path/to/credentials.json',
-        # Scopes for the gmail google API
-        'scopes': ['https://www.googleapis.com/auth/gmail.modify'],
-    }
+    'credentials_path': '/path/to/credentials.json',
+    # Scopes for the gmail google API
+    'scopes_for_sending': ['https://www.googleapis.com/auth/gmail.modify'],
+    'scopes_for_reading': ['https://www.googleapis.com/auth/gmail.modify'],
 }
 
 # smtp is used for sending (outgoing) messages
@@ -61,9 +48,7 @@ smtp_imap = {
     # Outgoing Mail (SMTP) Server
     'smtp_server': 'SMTP_SERVER_NAME',  # e.g. smtp.gmail.com
     # Incoming Mail (IMAP) Server
-    'imap_server': 'IMAP_SERVER_NAME',  # e.g. imap.gmail.com
-    # If no email password found locally, prompt email password
-    'prompt_email_password': True
+    'imap_server': 'IMAP_SERVER_NAME'  # e.g. imap.gmail.com
 }
 
 # ==================================
@@ -71,33 +56,30 @@ smtp_imap = {
 # ==================================
 # Config options for sending emails
 send_emails = {
-    # How to connect to the email server: googleapi or smtp_imap
-    'connection_method': 'smtp_imap',
     'receiver_email_address': 'receiver@mail.com',
     # Sign and encrypt in a single pass. Otherwise, sign first and then encrypt
     # as separate processes
-    'use_single_pass': False,
+    'use_single_pass': True,
     # Signature options
-    'signature': {
+    'sign': {
         'program': 'GPG',
-        'enable_signature': True,
+        'enable_signature': False,
+        # Your signature (USER-ID), e.g. fingerprint
+        'signature': 'YOUR_SIGNATURE'
     },
     # Encryption options
-    'encryption': {
+    'encrypt': {
         'program': 'GPG',
-        'encryption_type': 'asymmetric'
+        # Recipient's USER-ID, e.g. fingerprint
+        'recipient_userid': 'RECIPIENT_USERID'
     }
 }
 
 # Config options for reading emails
 read_emails = {
-    # How to connect to the email server for reading emails: googleapi or smtp_imap
-    'connection_method': 'googleapi',
     'add_decryption_results': False,
-    # Folders for saving emails
-    'valid_emails_dirpath': '/path/to/valid/emails/',
-    'invalid_emails_dirpath': '/path/to/invalid/emails/',
-    'unknown_emails_dirpath': '/path/to/unknown/emails/'
+    # Directory for saving emails
+    'emails_dirpath': '/path/to/emails'
 }
 
 # ===============
@@ -114,4 +96,4 @@ test_message = "Hello, World!"
 
 # Test connection to an email server either through googleapi, smtp_imap or None
 # If None, then no connection testing will done
-test_connection = 'googleapi'
+test_connection = None
